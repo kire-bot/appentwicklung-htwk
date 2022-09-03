a1a752450e006487763e56c593cc267e66b93bc2
Erik <erikmicha@yahoo.de>
2022-09-03 19:01:07 +0200

    Add and edit stuff

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,3 @@
 import { initDrawerClick } from './modules/drawer.js'
 
-let template = document.getElementById('foo')
-let templateContent = template.content
-document.body.appendChild(templateContent)
-
 initDrawerClick()
